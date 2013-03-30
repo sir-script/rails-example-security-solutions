d51e680c1045d6d0d43d5444d7339d33dc86a25d
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2013-03-30 21:33:54 +0100

    just articles

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Testanotherthest::Application.load_tasks
+HackMe::Application.load_tasks
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Testanotherthest::Application.config.session_store :cookie_store, key: '_testanotherthest_session'
+HackMe::Application.config.session_store :cookie_store, key: '_hackme_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Testanotherthest::Application.config.session_store :active_record_store
+# HackMe::Application.config.session_store :active_record_store
