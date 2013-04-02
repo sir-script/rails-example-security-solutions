2834a664f8ee0fd2d44cf25cb9ceeb24cbbd4e56
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2013-04-02 22:49:32 +0200

    set up db for travis

---
+++ config/initializers/secret_token.travis.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackMe::Application.config.secret_token = '1fafaa898af87af797af98098af098af9898a98af67af565af65a6f5676af67af7878af67457ddaee83214cbee9eaf511a8544816207bee18ded4e7b53c890ef'
